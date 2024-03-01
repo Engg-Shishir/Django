275e8df2adb2a132f28fb6bd5cd326302b5ebaba
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-03-02 00:27:43 +0600

    Setup a static portfolio website

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 urlpatterns = [
      path('',views.index,name="index"),
      path('about',views.about,name="about"),
-     path('reverse',views.reverse,name="reverse")
+     path('resume',views.resume,name="resume"),
+     path('portfolio',views.portfolio,name="portfolio"),
+     path('services',views.services,name="services"),
+     path('contact',views.contact,name="contact")
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -5,6 +5,14 @@ def index(request):
 
 def about(request):
     return render(request,'about.html')
+def resume(request):
+    return render(request,'resume.html')
+def portfolio(request):
+    return render(request,'portfolio.html')
+def services(request):
+    return render(request,'services.html')
+def contact(request):
+    return render(request,'contact.html')
 
 def reverse(request):
     if request.method=="POST":
