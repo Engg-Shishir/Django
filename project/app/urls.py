62a2454f0c665449800307309cd75a2f38d75cd5
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-03-01 16:34:11 +0600

    Create About page

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,6 @@ from app import views
 from django.urls import path
 
 urlpatterns = [
-     path('',views.index,name="index")
+     path('',views.index,name="index"),
+     path('about',views.about,name="about")
 ]
