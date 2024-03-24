55ec8d4df5b8ebf1025769519112a3946c8298c1
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-03-24 06:39:09 +0600

    Contact form save in database views file

--- project/app/views.py
+++ project/app/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 
-from .models import Author
+from .models import Author,Message
 # Create your views here.
 def index(request):
-    authors_with_social = Author.objects.prefetch_related('socials').all()
+    authors_with_social = Author.objects.prefetch_related('socials', 'projects', 'testimonials').all()
     return render(request,'index.html',{'data':authors_with_social})
 
 def about(request):
-    return render(request,'about.html')
+    authors_with_social = Author.objects.prefetch_related('socials','abouts').all()
+    return render(request,'about.html',{'data':authors_with_social})
+
 def resume(request):
-    return render(request,'resume.html')
+    authors_with_resume = Author.objects.prefetch_related('education', 'experiance').all()
+    return render(request,'resume.html',{'data':authors_with_resume})
+
 def portfolio(request):
     return render(request,'portfolio.html')
+
 def services(request):
-    return render(request,'services.html')
+    authors_with_services = Author.objects.prefetch_related('services').all()
+    return render(request,'services.html',{'data':authors_with_services})
+
 def contact(request):
-    return render(request,'contact.html')
+    authors_with_resume = Author.objects.prefetch_related('contacts').all()
+    return render(request,'contact.html',{'data':authors_with_resume})
+    
 
 def reverse(request):
     if request.method=="POST":
@@ -24,3 +33,17 @@ def reverse(request):
 
         dictionary={"input":input,"output":output}
     return render(request,'result.html',dictionary)
+
+
+def save(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        message=request.POST.get('message')
+        tb = Message(name=name,email=email,subject=subject,msg=message)
+        tb.save()
+
+
+    authors_with_resume = Author.objects.prefetch_related('contacts').all()
+    return render(request,'contact.html',{'data':authors_with_resume,'message':"Message send successfully !"})
